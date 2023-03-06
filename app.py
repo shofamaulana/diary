1685906c9ce1b6419d5146d013f8682fda7d2d9b
shofamaulana <shofamaulanaahmy@gmail.com>
2023-03-06 12:39:35 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient('mongodb+srv://shofa:shofa@cluster0.w1x1v1x.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbshofa
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({}, {'_id':False}))
+    return jsonify({'articles': articles})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}'
+    profile.save(profilename)
+
+    doc = {
+        'file': filename,
+        'profile': profilename,
+        'title': title_receive,
+        'content': content_receive
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'message': 'upload complete!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
